023feb006022fc1bdcd1945d7b13c364f05521b3
Maciej Witos <mac.witos@gmail.com>
2020-02-17 15:06:23 +0100

    Zadanie 1.5

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,11 +1,24 @@
 from datetime import datetime
-
+import random
 from django.shortcuts import render
 from django.views import View
+from jedzonko.models import *
 
 
 class IndexView(View):
 
     def get(self, request):
-        ctx = {"actual_date": datetime.now()}
-        return render(request, "index.html", ctx)
+        # ctx = {"actual_date": datetime.now()}
+        lenght = Recipe.objects.all().__len__()
+        lenght += 6
+        choice = random.randint(6, lenght)
+        recipe1 = Recipe.objects.filter(id=choice)
+        choice = random.randint(6, lenght)
+        recipe2 = Recipe.objects.filter(id=choice)
+        choice = random.randint(6, lenght)
+        recipe3 = Recipe.objects.filter(id=choice)
+        return render(request, "index.html",
+                      {"recipe1": recipe1,
+                      "recipe2": recipe2,
+                      "recipe3": recipe3}
+                      )
