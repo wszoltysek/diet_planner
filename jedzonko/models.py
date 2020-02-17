6e928d827dde4fac3639b2a704b388a00426abcc
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-02-17 12:49:07 +0100

    Zadanie 1.2

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+import datetime
 
 
-class Example (models.Model):
-    pass
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+    ingredients = models.TextField()
+    description = models.TextField()
+    created = models.DateField(datetime.datetime.now)
+    updated = models.DateField(datetime.datetime.now)
+    preparation_time = models.IntegerField()
+    votes = models.IntegerField(default=0)
+
+
--- jedzonko/tests.py
+++ jedzonko/tests.py
@@ -2,18 +2,3 @@ from django.test import TestCase
 
 # Create your tests here.
 
-# Test GITHUBA skadjfkdhgkdsngdzlfkdgnfjkj
-
-def to_jest_test():
-    pass
-
-
-def test_wojtka():
-    pass
-
-def test_gita():
-    pass
-
-def test_mackaa():
-    pass
-
