ada5d2277fefe7d1f0ef61d4f73e0171941313dc
Maciej Witos <mac.witos@gmail.com>
2020-02-17 12:33:02 +0100

    Zadanie 1.1

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -8,4 +8,4 @@ class IndexView(View):
 
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
-        return render(request, "test.html", ctx)
+        return render(request, "index.html", ctx)
