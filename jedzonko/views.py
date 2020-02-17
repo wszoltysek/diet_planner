af56146a44ea74b70ca4b87e84451ff2ccb5de37
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-02-17 13:53:23 +0100

    Zadanie 2.1

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -9,3 +9,7 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "index.html", ctx)
+
+class Dashboard(View):
+    def get(self, request):
+        return render(request, "dashboard.html")
--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from jedzonko.views import IndexView
+from jedzonko.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view()),
+    path('main/', Dashboard.as_view()),
 ]
