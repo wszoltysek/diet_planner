7a5319836b8ecfefe6ac6577557387d06cd67e15
Maciej Witos <mac.witos@gmail.com>
2020-02-20 16:47:01 +0100

    Modyfikacje na grupie

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import random
-from django.shortcuts import render, render_to_response, redirect
+from django.shortcuts import render, render_to_response, redirect, reverse
 from django.core.paginator import Paginator
 
 from django.db.models import F
@@ -140,7 +140,7 @@ class PlanAdd(View):
     def get(self, request):
         return render(request, "app-add-schedules.html")
 
-    def post(self, request, id):
+    def post(self, request):
         name = request.POST.get("name")
         description = request.POST.get("description")
         created = datetime.datetime.now()
@@ -148,7 +148,7 @@ class PlanAdd(View):
             new_plan = Plan.objects.create(name=name,
                                 description=description,
                                 created=created)
-            return redirect(reverse('app-details-schedules', kwargs={"id": new_plan.id}))
+            return redirect(f'/plan/{new_plan.id}')
         else:
             error_message = messages.info(request, "Nie podano wszystkich danych")
             return redirect("/plan/add/", {"error_message": error_message})
